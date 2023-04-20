3526b12f33bf26a138ae29ac848ba1ff2b729ef2
olehstep <olehstepanenkoh@gmail.com>
2023-04-20 17:55:36 -0400

    first commit

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   $('.announcement').slick({
+     infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay:true        
+  });
+
+});
